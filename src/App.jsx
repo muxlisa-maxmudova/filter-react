4ed58487b4b23ff76cbf95b4531a246e87670bc6
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-02-19 10:47:18 +0500

    sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './index.css';
 import Nav from "./Navigation/Nav.jsx";
 import Products from "./Products/Products.jsx";
 import Recommended from "./Recommended/Recommended.jsx";
+import Sidebar from "./Sidebar/Sidebar.jsx";
 
 const App = () => {
     return (
         <>
+            <Sidebar/>
             <Nav/>
             <Recommended/>
             <Products/>
--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,7 +1,19 @@
 import './Sidebar.css'
+import Category from "./Category/Category.jsx";
+import Price from "./Price/Price.jsx";
+import Color from "./Color/Color.jsx";
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <>
+           <section className={'sidebar'}>
+               <div className={'logo-container'}>
+                   <h1>🛒</h1>
+               </div>
+               <Category/>
+               <Price/>
+               <Color/>
+           </section>
+        </>
     )
 }
 export default Sidebar
