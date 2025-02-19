5e5fd10cd512170f00e8521e5e69ea5b5daf2ca2
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-02-19 11:17:06 +0500

    sidebar elements done

--- src/Sidebar/Color/Color.jsx
+++ src/Sidebar/Color/Color.jsx
@@ -1,7 +1,41 @@
 import './Color.css'
 const Color = () => {
     return (
-        <div>Color</div>
+        <>
+            <div>
+                <h2 className="sidebar-title color-title">Colors</h2>
+                <label className="sidebar-label-container">
+                    <input type="radio" value="" name="test1"/>
+                    <span className="checkmark all"></span>
+                    All
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" value="" name="test1"/>
+                    <span className="checkmark all"></span>
+                    Black
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" value="" name="test1"/>
+                    <span className="checkmark all"></span>
+                    Blue
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" value="" name="test1"/>
+                    <span className="checkmark all"></span>
+                    Red
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" value="" name="test1"/>
+                    <span className="checkmark all"></span>
+                    Green
+                </label>
+                <label className="sidebar-label-container">
+                    <input type="radio" value="" name="test1"/>
+                    <span className="checkmark all"></span>
+                    White
+                </label>
+            </div>
+        </>
     )
 }
 export default Color
