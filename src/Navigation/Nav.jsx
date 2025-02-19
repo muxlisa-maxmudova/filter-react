9272f4194a56309bb04afc62be610563e6b9a3f4
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-02-19 10:20:14 +0500

    design of nav and products done

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,7 +1,31 @@
 import './Nav.css'
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from "react-icons/ai";
+import {FiHeart} from "react-icons/fi";
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <div className='nav-container'>
+                <input
+                className='search-input'
+                type='text'
+                //onChange={handleInputChange}
+                //value={query}
+                placeholder='Enter search shoes'
+                />
+            </div>
+                <div className='profile-container'>
+                    <a href='#'>
+                       <FiHeart className='nav-icons'/>
+                    </a>
+                    <a href=''>
+                        <AiOutlineShoppingCart className='nav-icons'/>
+                    </a>
+                    <a href=''>
+                       <AiOutlineUserAdd className='nav-icons'/>
+                    </a>
+                </div>
+
+        </nav>
     )
 }
 export default Nav
